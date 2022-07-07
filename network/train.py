7742d026093f93071c3f52291bd7808a0f2f1ecd
Patrick Huang <huangpatrick16777216@gmail.com>
2022-07-07 08:24:18 -0700

    stuff

--- network/train.py
+++ network/train.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from tqdm import trange
 
 import torch
+from torch.optim.lr_scheduler import ExponentialLR
 from torch.utils.data import DataLoader, random_split
 
 from constants import *
@@ -29,6 +30,7 @@ def train(args, model):
 
     loss_fn = torch.nn.MSELoss()
     optim = torch.optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = ExponentialLR(optim, gamma=0.997)
     print(f"Using loss function {loss_fn}")
     print(f"Using optimizer {optim}")
     print(model)
@@ -44,10 +46,15 @@ def train(args, model):
             optim.zero_grad()
             loss.backward()
             optim.step()
+        scheduler.step()
 
-        msg = f"Epoch: {epoch}, Loss: {loss:.4f}"
+        msg = f"Epoch: {epoch}, Loss: {loss:.4f}, LR: {optim.param_groups[0]['lr']:.6f}"
         with open(args.log, "a") as f:
             f.write(msg + "\n")
+
+        if epoch % 100 == 0:
+            path = os.path.join("/home/patrick/stuff/cnnpbr", f"model_{epoch}.pth")
+            torch.save(model.state_dict(), path)
 
         # Compute average loss on test dataset
         avg_loss = 0
@@ -66,18 +73,18 @@ if __name__ == "__main__":
     parser.add_argument("--resume", action="store_true",
         help="Continue training from a previous model")
     parser.add_argument("--epochs", default=100, type=int, help="Number of epochs to train")
-    parser.add_argument("--batch-size", default=32, type=int, help="Batch size")
+    parser.add_argument("--batch-size", default=64, type=int, help="Batch size")
     parser.add_argument("--lr", default=1e-3, type=float, help="Learning rate")
     parser.add_argument("--log", default="train.log", help="Path to log file")
     args = parser.parse_args()
 
     print(f"Using device {DEVICE}")
 
-    model = CNNPBRModel(layers=LAYERS)
+    model = CNNPBRModel(layers=LAYERS, kernel=5)
     if args.resume:
         print("Loading model")
         model.load_state_dict(torch.load("model.pth"))
-    model = model.to(device)
+    model = model.to(DEVICE)
 
     losses = train(args, model)
 
