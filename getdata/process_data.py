8c93b34672178b089018d9675595f018b9c0b908
Patrick Huang <huangpatrick16777216@gmail.com>
2022-07-07 18:56:13 -0700

    Removed old.

--- getdata/process_data.py
+++ getdata/process_data.py
@@ -62,46 +62,5 @@ def main():
         threads[i] = thread
 
 
-        """
-        maps = {}
-        for f in os.listdir(dir):
-            if f.endswith(".jpg"):
-                img = cv2.imread(os.path.join(dir, f))
-                height, width = img.shape[:2]
-                if width > height:
-                    img = img[:, :height, :]
-                elif height > width:
-                    img = img[:width, :, :]
-                img = cv2.resize(img, (args.size, args.size))
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-                color_img = "color" in f.lower() or "normal" in f.lower()
-                if not color_img:
-                    img = np.sum(img, axis=2) / 3
-                    real = np.empty((img.shape[0], img.shape[1], 1), dtype=img.dtype)
-                    real[..., 0] = img
-                    img = real
-
-                img = img / 255
-                img = img.transpose((2, 0, 1))
-
-                name = os.path.splitext(f)[0]
-                maps[name] = img
-
-        color = torch.Tensor(maps["color"]).float()
-        with open(os.path.join(new, "input.tensor"), "wb") as f:
-            torch.save(color, f)
-
-        other = np.empty((5, args.size, args.size), dtype=np.float64)
-        other[0:3] = maps["normal"]
-        other[3] = maps["disp"]
-        other[4] = maps["rough"]
-
-        other = torch.from_numpy(other).float()
-        with open(os.path.join(new, "output.tensor"), "wb") as f:
-            torch.save(other, f)
-        """
-
-
 if __name__ == "__main__":
     main()
