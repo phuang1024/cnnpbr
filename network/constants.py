19b4c7c290fa80f1e625a76e3199763e1daed81a
Patrick Huang <huangpatrick16777216@gmail.com>
2022-07-14 12:39:20 -0700

    More layers.

--- network/constants.py
+++ network/constants.py
@@ -3,10 +3,10 @@ import torch
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 IMG_SIZE = 512
-LAYERS = 3
-KERNEL_SIZES = [3, 5, 5]
+LAYERS = 5
+KERNEL_SIZES = [3, 3, 5, 5, 5]
 CHANNELS_EXP = 2
-LU_ALPHA = 0.2
+LU_ALPHA = 1
 POOLING = torch.nn.MaxPool2d
 LR_INIT = 1e-3
 LR_DECAY = 0.95
