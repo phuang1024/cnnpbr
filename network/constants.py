5c474d72fc289fdd3f836d912d4c1a047005c660
Patrick Huang <huangpatrick16777216@gmail.com>
2022-07-13 19:11:47 -0700

    Test new kernel sizes.

--- network/constants.py
+++ network/constants.py
@@ -4,7 +4,7 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 IMG_SIZE = 512
 LAYERS = 3
-KERNEL_SIZES = [3, 5, 5]
+KERNEL_SIZES = [1, 3, 3]
 CHANNELS_EXP = 2
 LRELU_ALPHA = 0.2
 POOLING = torch.nn.MaxPool2d
