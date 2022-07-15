86ebdfaa54b9d8d654f52b01fd7550ab8ecc1ce4
Patrick Huang <huangpatrick16777216@gmail.com>
2022-07-14 20:56:28 -0700

    Write logfile.

--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import trange
@@ -13,6 +15,9 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def train_model(args):
+    with open(args.logfile, "w") as f:
+        f.write(f"Train start: {datetime.now()}\n")
+
     dataset = TextureDataset(args.data_path, True)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True,
         num_workers=args.data_workers, prefetch_factor=4)
@@ -24,6 +29,7 @@ def train_model(args):
     for epoch in (pbar := trange(args.epochs)):
         pbar.set_description("Training", refresh=True)
 
+        avg_loss = 0.0
         for i, (x, y) in enumerate(dataloader):
             msg = f"epoch {epoch + 1}/{args.epochs}, batch {i + 1}/{len(dataloader)}"
             pbar.set_description(msg, refresh=True)
@@ -34,7 +40,12 @@ def train_model(args):
             model.train()
             out = model(x)
             loss = loss_fn(out, y)
+            avg_loss += loss.item()
 
             optim.zero_grad()
             loss.backward()
             optim.step()
+
+        avg_loss /= len(dataloader)
+        with open(args.logfile, "a") as f:
+            f.write(f"epoch {epoch + 1}/{args.epochs}, avg_loss: {avg_loss}\n")
